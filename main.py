3f1ac1ff7a8631f22d0dc5ea7b3a11b24b10d6da
Roberto Herrera <robeherrera561.2@gmail.com>
2022-08-05 23:07:01 -0400

    - Add field in the HTML - Simple way to get the data from the POST Form

--- main.py
+++ main.py
@@ -5,33 +5,32 @@ from model import breast_model as model
 
 app = Flask(__name__, template_folder='./templates')
 
+# Point of improvement
+# 1- Have multiple data sets to change in the dropdown menu
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/post', methods=['POST'])
+
+@app.route('/post', methods=['GET', 'POST'])
 def ai_model():
     if request.method == "POST":
+        # TODO Rober start here tomorrow
+        # 1 - Create a list
+        # 2 - List to Pandas DataFrame
+        # 3 - Send the DF to model
+        # 4 - Print Prediction
+        # 5 - Display Prediction in the HTML
+
         # getting input with name = fname in HTML form
-        first_name = request.form.get("name")
+        first_name = request.form.get("radious_mean")
         # getting input with name = lname in HTML form
-        email = request.form.get("email")
-        return "Your name is "+ first_name + " " + email
+        email = request.form.get("texture_mean")
+        return "Your name is " + str(first_name) + " " + str(email)
     return "recived: {}".format(request.form)
 
 
-# TODO Robert: delete this, only for demostration proposess
-@app.route("/<celsius>")
-def fahrenheit_from(celsius):
-    """Convert Celsius to Fahrenheit degrees."""
-    try:
-        fahrenheit = float(celsius) * 9 / 5 + 32
-        fahrenheit = round(fahrenheit, 3)  # Round to three decimal places
-        return str(fahrenheit)
-    except ValueError:
-        return "invalid input"
-
-
 if __name__ == "__main__":
-    # app.run(host="127.0.0.1", port=8080, debug=True)
     model.run()
+    app.run(host="127.0.0.1", port=8080, debug=True)
